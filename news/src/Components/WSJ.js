dd78c35de932203ae143ba674af0d7e14ec8041c
vanceleon <vanceleon44@gmail.com>
2019-02-08 19:42:56 -0500

    latest

--- news/src/Components/WSJ.js
+++ news/src/Components/WSJ.js
@@ -6,36 +6,54 @@ class WSJ extends Component {
   constructor() {
     super();
     this.state = {
-      news: [],
+      wsjNews: [],
       URL:
         'https://newsapi.org/v2/top-headlines?sources=the-wall-street-journal&apiKey=',
-      wsj_key: process.env.REACT_APP_NEWS_API,
+      wsj_key: process.env.REACT_APP_NEWS_API
     };
   }
 
   componentDidMount() {
     this.getNews();
-    // console.log(this.state.news);
+  }
+
+  componentDidUpdate(prevState) {
+    if (
+      this.state.wsjNews !== prevState.wsjNews ||
+      this.state.wsjNews.articles.length > 0
+    ) {
+      console.log('update state', this.state.wsjNews);
+    }
   }
 
   getNews = () => {
-    const url2 = `${this.state.URL}${this.state.wsj_key}`
+    const url2 = `${this.state.URL}${this.state.wsj_key}`;
     axios
       .get(url2)
       .then(res => {
         const wsj_news = res.data;
-        console.log(wsj_news);
-        this.setState({ news: wsj_news });
+        // console.log(wsj_news);
+        this.setState({ wsjNews: wsj_news });
+        // {console.log(this.state.wsjNews.articles[0].author)}
       })
       .catch(err => console.log('Error', err));
   };
 
   render() {
-    return (
-      <React.Fragment> 
+    // const newsArticles = this.state.wsjNews.articles
+    // console.log(newsArticles)
+    {this.state.wsjNews.articles.map((news, i) => {
+      return <li key={i}>{news.author}</li>;
+    })}
 
-      </React.Fragment>
-    )
+    return (
+      <ul className='wsj-Cards'>
+        {/* {console.log(this.state.wsjNews.articles)} */}
+        {/* <ul>{newsArticles}</ul> */}
+        hello world
+        {/* <h1>{this.state.wsjNews.articles[0].author}</h1>  */}
+      </ul>
+    );
   }
 }
 
