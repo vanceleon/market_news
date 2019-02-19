6c452ff1ac889a134c029d862de4dc39ac5e3d46
vanceleon <vanceleon44@gmail.com>
2019-02-19 14:32:07 -0500

    rendering news from wsj field yields cnbc and tech crunch

--- news/src/App.js
+++ news/src/App.js
@@ -11,23 +11,22 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar/>
-        <h1>Fed Yield Curves</h1>
         <InterestRates/>
-        {/* <br></br>
+        <br></br>
         <h1>WSJ</h1>
         <WSJ/>
         <h1>CNBC News</h1>
         <CNBC/>
         <h1>Tech Cruch</h1>
         <TechCrunch/>
-        <h1>World News</h1>
+        {/* <h1>World News</h1>
         <h1>Briefing.com</h1>
         <h1>Trading Econonics</h1>
         <h1>Currency Overview</h1>
         <h1>Major Indexes</h1>
         <h1>Municipal Bonds</h1>
         <h1>Corporate Bonds</h1>
-        <h1>Commondities Overview</h1> */}
+        <h1>Commondities Overview</h1>  */}
       </div>
     );
   }
