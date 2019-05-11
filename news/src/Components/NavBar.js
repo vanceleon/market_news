1f276d96920bad977464c8a153e576a6f6e33ec8
vanceleon <vanceleon44@gmail.com>
2019-05-11 15:40:23 -0400

    formatting

--- news/src/Components/NavBar.js
+++ news/src/Components/NavBar.js
@@ -1,41 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 class NavBar extends Component {
-    constructor() {
-        super();
-        this.state = {
-            event: false
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      event: false
+    };
+  }
 
+  onClick = () => {
+    this.setState({ event: true });
+    window.addEventListener('click', this.handleEvent);
+  };
 
-    onClick = () => {
-        this.setState({event: true});
-        window.addEventListener('click', this.handleEvent);
-    }
+  handleEvent = ev => {
+    ev.preventDefault();
+  };
 
-
-    handleEvent = (ev) => {
-        ev.preventDefault();
-
-    }
-
-    render() {
-        return (
-            <div style={{display:'flex', justifyContent:'center', margin: '15px 0'}}>
-                <h1>Morning Briefing</h1> 
-                <a className="navLinks" href='#interestRates'>Interest Rates</a> 
-                <div>WSJ</div> 
-                <div>CNBC</div> 
-                <div>Tech Crunch</div> 
-            </div>   
-           )
-
-    }
+  render() {
+    return (
+      <div
+        style={{ display: 'flex', justifyContent: 'center', margin: '15px 0' }}
+      >
+        <h1>Morning Briefing</h1>
+        <a className='navLinks' href='#interestRates'>
+          Interest Rates
+        </a>
+        <div>WSJ</div>
+        <div>CNBC</div>
+        <div>Tech Crunch</div>
+      </div>
+    );
+  }
 }
 
 export default NavBar;
 // const NavBar = () => {
-    
-// }
 
+// }
