817b40b56f5c109d4284a07e80ee493590102999
vanceleon <vanceleon44@gmail.com>
2019-05-11 15:26:17 -0400

    adding links to the nav

--- news/src/Components/NavBar.js
+++ news/src/Components/NavBar.js
@@ -3,7 +3,11 @@ import React from 'react';
 const NavBar = () => {
     return (
      <div style={{display:'flex', justifyContent:'center', margin: '15px 0'}}>
-         <h1>Morning Brief</h1> 
+         <h1>Morning Briefing</h1> 
+         <div>Interest Rates</div> 
+         <div>WSJ</div> 
+         <div>CNBC</div> 
+         <div>Tech Crunch</div> 
      </div>   
     )
 }
