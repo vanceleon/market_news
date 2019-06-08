4b28178c30733dd1f802221c86417e9d086ce237
vanceleon <vanceleon44@gmail.com>
2019-06-08 09:43:46 -0400

    adding a container card for colapsable styling will shorten the initial scrolling for user

--- news/src/Components/WSJ.js
+++ news/src/Components/WSJ.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import {Container, Header} from 'semantic-ui-react';
+import { Container, Header } from 'semantic-ui-react';
 import axios from 'axios';
-
 
 class WSJ extends Component {
   constructor() {
     super();
     this.state = {
       wsjNews: [],
+      firstCard: [],
       URL:
         'https://newsapi.org/v2/top-headlines?sources=the-wall-street-journal&apiKey=',
       wsj_key: process.env.REACT_APP_NEWS_API
@@ -18,53 +18,77 @@ class WSJ extends Component {
     this.getNews();
   }
 
-
   getNews = () => {
     const URL = `${this.state.URL}${this.state.wsj_key}`;
     axios
       .get(URL)
       .then(res => {
         const wsj_news = res.data;
-        // console.log(wsj_news);
-        this.setState({ wsjNews: wsj_news });
-
+        console.log('array of wsyj', wsj_news.articles[0]);
+        this.setState({ wsjNews: wsj_news, firstCard: wsj_news.articles[0] });
       })
       .catch(err => console.log('Error', err));
   };
 
   render() {
-
     if (this.state.wsjNews.articles) {
       return (
         <Container text>
-        <Header as='h1' id='WSJ'>
-          WSJ
+          <Header as='h1' id='WSJ'>
+            WSJ
           </Header>
-        <div className='newsCards'>
-          {console.log(this.state.wsjNews.articles)}
-          {this.state.wsjNews.articles.map((news, i) => {
-            return (
-              <div key={i} className="article" style={{margin: '20px 0'}}>
-                <a href={news.url}><img src={news.urlToImage } alt="news-highlights" style={{width: '100%', borderRadius: '15px'}}/></a>
-                <a href={news.url}>{news.title}</a>
-                <div style={{margin: '10px 0 0 0'}}>{news.description}</div>
-                <div style={{fontStyle: 'italic'}}>Author: {news.author}</div>
+          <div className='newsCards'>
+            <div className='article' style={{ margin: '20px 0' }}>
+              <a href={this.state.firstCard.url}>
+                <img
+                  src={this.state.firstCard.urlToImage}
+                  alt='news-highlights'
+                  style={{ width: '100%', borderRadius: '15px' }}
+                />
+              </a>
+              <a href={this.state.firstCard.url}>
+                {this.state.firstCard.title}
+              </a>
+              <div style={{ margin: '10px 0 0 0' }}>
+                {this.state.firstCard.description}
               </div>
-            );
-          })}
-        </div>
-
+              <div style={{ fontStyle: 'italic' }}>
+                Author: {this.state.firstCard.author}
+              </div>
+            </div>
+          </div>
+          <div className='newsCards'>
+            {console.log(this.state.wsjNews.articles)}
+            {this.state.wsjNews.articles.map((news, i) => {
+              return (
+                <div key={i} className='article' style={{ margin: '20px 0' }}>
+                  <a href={news.url}>
+                    <img
+                      src={news.urlToImage}
+                      alt='news-highlights'
+                      style={{ width: '100%', borderRadius: '15px' }}
+                    />
+                  </a>
+                  <a href={news.url}>{news.title}</a>
+                  <div style={{ margin: '10px 0 0 0' }}>{news.description}</div>
+                  <div style={{ fontStyle: 'italic' }}>
+                    Author: {news.author}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </Container>
       );
     } else {
       return (
-      <div class="ui segment">
-  <div class="ui active inverted dimmer">
-    <div class="ui text loader">Loading</div>
-  </div>
-  <p></p>
-</div>
-        )
+        <div class='ui segment'>
+          <div class='ui active inverted dimmer'>
+            <div class='ui text loader'>Loading</div>
+          </div>
+          <p />
+        </div>
+      );
     }
   }
 }
