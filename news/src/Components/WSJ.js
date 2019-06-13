5de09be7f687a3a9cb02303ee976a993db479ef9
vanceleon <vanceleon44@gmail.com>
2019-06-13 10:08:17 -0400

    completed functional component working on transfering code to other class components"

--- news/src/Components/WSJ.js
+++ news/src/Components/WSJ.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Container, Header } from 'semantic-ui-react';
 import axios from 'axios';
+import CentralNewsFuncComponent from './NewsFunc'; 
 
 class WSJ extends Component {
   constructor() {
     super();
     this.state = {
+      newsHeader: 'WSJ',
       wsjNews: [],
       firstCard: [],
       URL:
@@ -24,81 +25,27 @@ class WSJ extends Component {
       .get(URL)
       .then(res => {
         const wsj_news = res.data;
-        console.log('array of wsj', wsj_news.articles[0]);
         this.setState({ wsjNews: wsj_news, firstCard: wsj_news.articles[0] });
       })
       .catch(err => console.log('Error', err));
   };
 
-
-onCardClick = event => {
-  event.preventDefault();
-  const collapseCard = document.getElementsByClassName("collabpsibleCard")
-  console.log("I clicked the card");
-} 
-
-
+  onCardClick = event => {
+    event.preventDefault();
+    const collapseCard = document.getElementsByClassName('collabpsibleCard');
+    console.log('I clicked the card');
+  };
 
   render() {
-    if (this.state.wsjNews.articles) {
-      return (
-        <Container text>
-          <Header as='h1' id='WSJ'>
-            WSJ
-          </Header>
-          <div className='firstContainerCard' onCLick={(event) => this.onCardClick(event)}>
-            <div className='article' style={{ margin: '20px 0' }}>
-              <a href={this.state.firstCard.url}>
-                <img
-                  src={this.state.firstCard.urlToImage}
-                  alt='news-highlights'
-                  style={{ width: '100%', borderRadius: '15px' }}
-                />
-              </a>
-              <a href={this.state.firstCard.url}>
-                {this.state.firstCard.title}
-              </a>
-              <div style={{ margin: '10px 0 0 0' }}>
-                {this.state.firstCard.description}
-              </div>
-              <div style={{ fontStyle: 'italic' }}>
-                Author: {this.state.firstCard.author}
-              </div>
-            </div>
-          </div>
-          <div className='newsCards'>
-            {console.log(this.state.wsjNews.articles)}
-            {this.state.wsjNews.articles.map((news, i) => {
-              return (
-                <div key={i} className='article' style={{ margin: '20px 0' }}>
-                  <a href={news.url}>
-                    <img
-                      src={news.urlToImage}
-                      alt='news-highlights'
-                      style={{ width: '100%', borderRadius: '15px' }}
-                    />
-                  </a>
-                  <a href={news.url}>{news.title}</a>
-                  <div style={{ margin: '10px 0 0 0' }}>{news.description}</div>
-                  <div style={{ fontStyle: 'italic' }}>
-                    Author: {news.author}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </Container>
-      );
-    } else {
-      return (
-        <div class='ui segment'>
-          <div class='ui active inverted dimmer'>
-            <div class='ui text loader'>Loading</div>
-          </div>
-          <p />
-        </div>
-      );
-    }
+    return (
+      <CentralNewsFuncComponent
+        newsHeader={this.state.newsHeader}
+        newsInfo={this.state.wsjNews}
+        firstCard={this.state.firstCard}
+        getNews={this.getNews}
+        onCardClick={this.onCardClick}
+      />
+    )
   }
 }
 
---
+++ news/src/Components/newsFunc.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Container, Header } from 'semantic-ui-react';
+
+
+const CentralNewsFuncComponent = props => {
+  if (props.newsInfo.articles) {
+    return (
+      <Container text>
+        <Header as='h1' id='WSJ'>
+          {props.newsHeader}
+        </Header>
+        <div
+          className='firstContainerCard'
+          onCLick={event => props.onCardClick(event)}
+        >
+          <div className='article' style={{ margin: '20px 0' }}>
+            <a href={props.firstCard.url}>
+              <img
+                src={props.firstCard.urlToImage}
+                alt='news-highlights'
+                style={{ width: '100%', borderRadius: '15px' }}
+              />
+            </a>
+            <a href={props.firstCard.url}>{props.firstCard.title}</a>
+            <div style={{ margin: '10px 0 0 0' }}>
+              {props.firstCard.description}
+            </div>
+            <div style={{ fontStyle: 'italic' }}>
+              Author: {props.firstCard.author}
+            </div>
+          </div>
+        </div>
+        <div className='newsCards'>
+          {console.log(props.newsInfo.articles)}
+          {props.newsInfo.articles.map((news, i) => {
+            return (
+              <div key={i} className='article' style={{ margin: '20px 0' }}>
+                <a href={news.url}>
+                  <img
+                    src={news.urlToImage}
+                    alt='news-highlights'
+                    style={{ width: '100%', borderRadius: '15px' }}
+                  />
+                </a>
+                <a href={news.url}>{news.title}</a>
+                <div style={{ margin: '10px 0 0 0' }}>{news.description}</div>
+                <div style={{ fontStyle: 'italic' }}>Author: {news.author}</div>
+              </div>
+            );
+          })}
+        </div>
+      </Container>
+    );
+  } else {
+    return (
+      <div class='ui segment'>
+        <div class='ui active inverted dimmer'>
+          <div class='ui text loader'>Loading</div>
+        </div>
+        <p />
+      </div>
+    );
+  }
+};
+
+export default CentralNewsFuncComponent;
