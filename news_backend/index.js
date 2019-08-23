11a415731284ad84c58af536ae0e5b96322cbab2
Vance Leon <vanceleon44@gmail.com>
2019-08-23 18:58:33 -0400

    scrapping author and article title of amzn page

--- news_backend/index.js
+++ news_backend/index.js
@@ -1,38 +1,47 @@
 const Nightmare = require('nightmare');
 const cheerio = require('cheerio');
 
-
-const nightmare = Nightmare({show: true});
+const nightmare = Nightmare({ show: false });
 const url = 'https://seekingalpha.com/';
 
-
 nightmare
-    .goto(url)
-    .wait('body')
-    .type(`input[type='search']`, 'AMZN' )
-    .click(`button.btn[type='submit']`)
-    .wait('app-root.ng-tns-c0-0')
-    .evaluate(() => document.querySelector('body').innerHTML)
-    .end()
-    .then(res => {
-        console.log(getData(res))
-    }).catch(err => {
-        console.log(err)
-    });
+  .goto(url)
+  .wait('body')
+  .type(`input[type='search']`, 'AMZN')
+  .click(`button.btn[type='submit']`)
+  .wait('div.content_section')
+  .evaluate(() => document.querySelector('body').innerHTML)
+  .end()
+  .then(res => {
+    console.log(getData(res));
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
-
-    let getData = html => {
-        data = [];
-        const $ = cheerio.load(html);
-        // console.log("what is $", $);
-        const raw_element = $('main section section div app-the-big-picture app-html-loader span.ng-star-inserted div')
-        let title = $(raw_element).find('div div').text();
-        // console.log(raw_element)
-        // console.log(title)
-        if(title) {
-            data.push({
-                title: title
-            });
+let getData = html => {
+  // console.log(html)
+  data = [];
+  const $ = cheerio.load(html);
+  $(
+    'div.symbol_articles_list div.portfolio_selections div.content_block_investment_views'
+  ).each((row, row_element) => {
+    $(row_element)
+      .find('ul li')
+      .each((i, elem) => {
+        let author = $(elem)
+          .find('a.small_picture')
+          .attr('href');
+        let title = $(elem)
+          .find('div.symbol_article')
+          .text();
+        if (author) {
+          data.push({
+            author: author,
+            title: title
+          });
         }
-        return data;
-    }
+      });
+  });
+  return data;
+};
