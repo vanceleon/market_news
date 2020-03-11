fa901bdf8a2393784237331622f9eb7ef48aa4df
vanceleon <vanceleon44@gmail.com>
2020-03-11 15:40:19 -0400

    so close

--- news/src/Components/functions/interestData.js
+++ news/src/Components/functions/interestData.js
@@ -3,78 +3,36 @@ export const interestDataOrganizer = interestData => {
   const data = [];
 
   let i = 0;
+  // for (let i = 0; i < interestData.data.length; i++) {
+  //   console.log(interestData.data[i][0])
+  // }
   while (i < interestData.column_names.length) {
-    if (i !== 0) {
-      // console.log(interestData.column_names[i]);
-      let j = 1;
-      data[i - j] = {};
-      data[i - j].name = interestData.column_names[i];
+    // console.log('before if',typeof(interestData.column_names[i]))
 
-      console.log('insertion to data', data);
-      let date = interestData.data[0][0];
-      for (let x = 1; x < interestData.data.length; x++) {
-        data[i - j][date] = interestData.data[x][x];
+    // console.log(interestData.column_names[i]);
+    data[i] = {};
+    data[i].name = interestData.column_names[i];
+    console.log('name insertion', data);
+    // console.log('insertion to data', data);
+    console.log('i', i, interestData.data[i]);
+    // let date = interestData.data[i - 1][0];
+    // date = date[0]
+    for (let j = 0; j < interestData.data.length; j++) {
+      let date = interestData.data[j][0];
+      // console.log('date', date);
+      for (let y = 0; y < interestData.data[j].length; y++) {
+        console.log('y', interestData.data[j][y]);
+        // let monthIterator = i
+        data[i][date] = interestData.data[j][y]
+        // monthIterator++
       }
-      console.log('after date insert', data);
+      // console.log()
     }
+    // console.log('after date insert', data);
+    // }
     i++;
   }
   console.log('result', data);
-
-  // interestData.data.map((interestRates, i) => {
-  //   console.log('array of ', interestRates);
-  //   for (let x = 0; x < interestRates.length; x++) {
-  //     // console.log("date", date)
-  //     if (x > 0) {
-  //       // data[i - j][date] = interestRates[x];
-  //     }
-  //   }
-  // });
-  // interestData.column_names.map((name, i) => {
-  // need to loop two more times for 20 and 30 year yield
-  // need to assign date and value to each ytm date
-  // explore looping through each data point and assign the values then dataObj[date] = looped through data points.
-  // console.log("what names", name,"at position", i)
-  // for(let j = 0; j < interestData.data[j][j].length; j++){
-  //   let date = interestData.data[i][j]
-  //   console.log(date)
-  //   if (i > 0) {
-  //     data[i] = { name, [date]: interestData.data[i][j] };
-  //   }
-  //   // if(j === 0){
-  //     // console.log(interestData.data[j])
-  //     console.log('what is the data obj here',data)
-
-  //   // }
-  // }
-  // });
-
-  // console.log('insert dates', data);
-  // // interestData.data.slice(0,10).map((daily,i) => {
-  // //   // data[i] = daily
-  // //   data.daily[0]
-
-  // //   console.log(daily)
-  // //   // return daily.map(rate => {
-  // //   //   return
-  // //   // })
-  // // })
-  // console.log('name column ', data);
-  // return data; //array of objects
 };
 
-// daily = date from example (uv, pv, amt)
-// uv = interest rate
-//         {interestRateData.data.slice(0, 10).map(daily => {
-//           return (
-//             <tr>
-//               {daily.map(rate => {
-//                 return <td>{rate}</td>;
-//               })}
-//             </tr>
-//           );
-//         })}
-//       </tbody>
-//     </table>
-//   </div>
-// </Container> */}
+// if (interestData.column_names[i] !== 'Date') { //this needs to be changed
