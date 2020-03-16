d29951273fa46025cf5587f697a2096d3145b6a6
vanceleon <vanceleon44@gmail.com>
2020-03-15 23:12:02 -0400

    centered interest rate table

--- news/src/Components/InterestRates.js
+++ news/src/Components/InterestRates.js
@@ -34,20 +34,20 @@ class InterestRates extends Component {
     axios
       .get(URL)
       .then(res => {
-    const preInterestRateData = res.data.dataset;
-    const interestData = interestDataOrganizer(preInterestRateData);
-    this.setState({
-      fedInt: interestData,
-      loaded: true,
-      fedData: preInterestRateData
-    });
-    })
-    .catch(err => console.log('Error', err));
+        const preInterestRateData = res.data.dataset;
+        const interestData = interestDataOrganizer(preInterestRateData);
+        this.setState({
+          fedInt: interestData,
+          loaded: true,
+          fedData: preInterestRateData
+        });
+      })
+      .catch(err => console.log('Error', err));
   };
 
   render() {
     if (this.state.loaded) {
-      let data = [0, 4,4,6,5]
+      let data = [0, 4, 4, 6, 5];
       data = this.state.fedInt;
       const dates = [];
       let i = 0;
@@ -60,31 +60,37 @@ class InterestRates extends Component {
 
       return (
         <div className='interest-rate-table-container'>
-          <Header as='h1'>Treasury Yield Rates</Header>        
-          <LineChart
-            width={600}
-            height={300}
-            data={data}
-            className='interest-chart'
-          >
-            <CartesianGrid strokeDasharray='3 3' />
-            <Legend />
-            <XAxis dataKey='name' />
-            <YAxis />
-            <Tooltip />
-             <Line type='monotone' dataKey={dates[1]} stroke='#B32C2C' strokeWidth={4} />
-            <Line type='monotone' dataKey={dates[2]} stroke='#82ca9d' />
-            <Line type='monotone' dataKey={dates[3]} stroke='#003f5c' />
-            <Line type='monotone' dataKey={dates[7]} stroke='#ffa600' />
-            <Line type='monotone' dataKey={dates[10]} stroke='#ff7c43' />
-            <Line type='monotone' dataKey={dates[15]} stroke='#c1e7ff' /> 
-            <Line type='monotone' dataKey={dates[20]} stroke='#95A5A5' /> 
-            <Line type='monotone' dataKey={dates[30]} stroke='#8F43AD' /> 
-            <Line type='monotone' dataKey={dates[45]} stroke='#D25400' /> 
-            <Line type='monotone' dataKey={dates[60]} stroke='#F39B11' /> 
-            <Line type='monotone' dataKey={dates[90]} stroke='#2D3E50' strokeWidth={4}/> 
-          </LineChart> 
-           
+          <Header as='h1'>Treasury Yield Rates</Header>
+          <div className='chart-container'>
+            <LineChart height={300} width={600} data={data}>
+              <CartesianGrid strokeDasharray='3 3' />
+              <Legend />
+              <XAxis dataKey='name' />
+              <YAxis />
+              <Tooltip />
+              <Line
+                type='monotone'
+                dataKey={dates[1]}
+                stroke='#B32C2C'
+                strokeWidth={4}
+              />
+              <Line type='monotone' dataKey={dates[2]} stroke='#82ca9d' />
+              <Line type='monotone' dataKey={dates[3]} stroke='#003f5c' />
+              <Line type='monotone' dataKey={dates[7]} stroke='#ffa600' />
+              <Line type='monotone' dataKey={dates[10]} stroke='#ff7c43' />
+              <Line type='monotone' dataKey={dates[15]} stroke='#c1e7ff' />
+              <Line type='monotone' dataKey={dates[20]} stroke='#95A5A5' />
+              <Line type='monotone' dataKey={dates[30]} stroke='#8F43AD' />
+              <Line type='monotone' dataKey={dates[45]} stroke='#D25400' />
+              <Line type='monotone' dataKey={dates[60]} stroke='#F39B11' />
+              <Line
+                type='monotone'
+                dataKey={dates[90]}
+                stroke='#2D3E50'
+                strokeWidth={4}
+              />
+            </LineChart>
+          </div>
         </div>
       );
     } else {
